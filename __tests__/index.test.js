e9a5bd078dff756d61bef21dfcb0d87f7ddb38de
ajsen <ajsmartmin@gmail.com>
2023-09-12 02:56:23 +0900

    Create index.test.js

---
+++ __tests__/index.test.js
@@ -0,0 +1,27 @@
+import genDiff from '../src/index.js';
+
+test('genDiff', () => {
+  const path1 = '__fixtures__/file_1.json';
+  const path2 = '__fixtures__/file_2.json';
+  const result = genDiff(path1, path2);
+  expect(result).toEqual({
+    '- follow': false,
+    host: 'hexlet.io',
+    '- proxy': '123.234.53.22',
+    '- timeout': 50,
+    '+ timeout': 20,
+    '+ verbose': true,
+  });
+});
+
+test('If the file or directory does not exist', () => {
+  const path1 = '__fixtures__/file-1.json';
+  const path2 = '_fixtures_/file_2.json';
+  expect(() => genDiff(path1, path2)).toThrow(new Error('No such file or directory'));
+});
+
+test('if the file is not json', () => {
+  const path1 = '__fixtures__/file_1.json';
+  const path2 = '__fixtures__/file.txt';
+  expect(() => genDiff(path1, path2)).toThrow(new Error('The extension must be json'));
+});
