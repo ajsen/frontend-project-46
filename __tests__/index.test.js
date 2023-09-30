d6fef9dce34e5ac9d933c534f5fe5c6120529135
ajsen <ajsmartmin@gmail.com>
2023-09-30 21:41:24 +0900

    Test: Fix tests for nested structures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,30 +1,23 @@
+import path from 'node:path';
+import fs from 'node:fs';
 import genDiff from '../src/index.js';
 
-test('Test json files', () => {
-  const path1 = '__fixtures__/file_1.json';
-  const path2 = '__fixtures__/file_2.json';
-  const result = genDiff(path1, path2);
-  expect(result).toEqual({
-    '- follow': false,
-    host: 'hexlet.io',
-    '- proxy': '123.234.53.22',
-    '- timeout': 50,
-    '+ timeout': 20,
-    '+ verbose': true,
+describe('Stylish output check', () => {
+  const outputPath = '__fixtures__/stylish_output';
+  const absolutePath = path.resolve(process.cwd(), outputPath);
+  const stylishOutput = fs.readFileSync(absolutePath, 'utf-8');
+  test('JSON files check', () => {
+    const path1 = '__fixtures__/file_1.json';
+    const path2 = '__fixtures__/file_2.json';
+    const result = genDiff(path1, path2, 'stylish');
+    expect(result).toEqual(stylishOutput);
   });
-});
 
-test('Test yaml files', () => {
-  const path1 = '__fixtures__/file_1.yml';
-  const path2 = '__fixtures__/file_2.yml';
-  const result = genDiff(path1, path2);
-  expect(result).toEqual({
-    '- follow': false,
-    host: 'hexlet.io',
-    '- proxy': '123.234.53.22',
-    '- timeout': 50,
-    '+ timeout': 20,
-    '+ verbose': true,
+  test('YAML files check', () => {
+    const path1 = '__fixtures__/file_1.yml';
+    const path2 = '__fixtures__/file_2.yml';
+    const result = genDiff(path1, path2, 'stylish');
+    expect(result).toEqual(stylishOutput);
   });
 });
 
