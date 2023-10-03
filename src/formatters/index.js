9e9c0e9c36bcc0818956faa1eac555ea77c594a0
ajsen <ajsmartmin@gmail.com>
2023-10-04 04:08:43 +0900

    Feat: Add JSON output

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,6 +1,16 @@
 import formatStylish from './stylish.js';
 import formatPlain from './plain.js';
+import formatJSON from './json.js';
 
-export default (data, formatName) => ((formatName === 'stylish')
-  ? formatStylish(data)
-  : formatPlain(data));
+export default (data, formatName) => {
+  switch (formatName) {
+    case 'stylish':
+      return formatStylish(data);
+    case 'plain':
+      return formatPlain(data);
+    case 'json':
+      return formatJSON(data);
+    default:
+      throw new Error('No such formatter');
+  }
+};
