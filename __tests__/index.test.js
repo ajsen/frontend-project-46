9664a74be41921a3865d95aeff635d0cb7ade04c
Ajsen Martynov <ajsmartmin@gmail.com>
2023-10-04 22:26:35 +0900

    Test: Refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,57 +1,29 @@
 import path from 'node:path';
 import fs from 'node:fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import genDiff from '../src/index.js';
 
-const pathToJSON1 = '__fixtures__/file_1.json';
-const pathToJSON2 = '__fixtures__/file_2.json';
-const pathToYAML1 = '__fixtures__/file_1.yml';
-const pathToYAML2 = '__fixtures__/file_2.yml';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-describe('Stylish output check', () => {
-  const outputPath = '__fixtures__/stylish_output.txt';
-  const absolutePath = path.resolve(process.cwd(), outputPath);
-  const stylishOutput = fs.readFileSync(absolutePath, 'utf-8');
-  test('JSON files check', () => {
-    const result = genDiff(pathToJSON1, pathToJSON2, 'stylish');
-    expect(result).toEqual(stylishOutput);
-  });
+const outputFormats = ['stylish', 'plain', 'json'];
+const dataFormats = ['json', 'yaml', 'yml'];
 
-  test('YAML files check', () => {
-    const result = genDiff(pathToYAML1, pathToYAML2, 'stylish');
-    expect(result).toEqual(stylishOutput);
-  });
-});
-
-describe('Plain output check', () => {
-  const outputPath = '__fixtures__/plain_output.txt';
-  const absolutePath = path.resolve(process.cwd(), outputPath);
-  const plainOutput = fs.readFileSync(absolutePath, 'utf-8');
-  test('JSON files check', () => {
-    const result = genDiff(pathToJSON1, pathToJSON2, 'plain');
-    expect(result).toEqual(plainOutput);
-  });
-  test('YAML files check', () => {
-    const result = genDiff(pathToYAML1, pathToYAML2, 'plain');
-    expect(result).toEqual(plainOutput);
-  });
-});
-
-describe('JSON output check', () => {
-  const outputPath = '__fixtures__/json_output.txt';
-  const absolutePath = path.resolve(process.cwd(), outputPath);
-  const jsonOutput = fs.readFileSync(absolutePath, 'utf-8');
-  test('JSON files check', () => {
-    const result = genDiff(pathToJSON1, pathToJSON2, 'json');
-    expect(result).toEqual(jsonOutput);
-  });
-  test('YAML files check', () => {
-    const result = genDiff(pathToYAML1, pathToYAML2, 'json');
-    expect(result).toEqual(jsonOutput);
+describe.each(outputFormats)('Check %s output format', (outputFormat) => {
+  test.each(dataFormats)('Check %s data format', (dataFormat) => {
+    const inputPath1 = getFixturePath(`file-1.${dataFormat}`);
+    const inputPath2 = getFixturePath(`file-2.${dataFormat}`);
+    const expectedOutput = readFixture(`${outputFormat}.txt`);
+    const result = genDiff(inputPath1, inputPath2, outputFormat);
+    expect(result).toEqual(expectedOutput);
   });
 });
 
 test('Test unsupported data format', () => {
-  const path1 = '__fixtures__/file_1.json';
-  const path2 = '__fixtures__/unsupported_format.txt';
+  const path1 = getFixturePath('file-1.json');
+  const path2 = getFixturePath('unsupported_format.txt');
   expect(() => genDiff(path1, path2)).toThrow(new Error('Unsupported data format'));
 });
