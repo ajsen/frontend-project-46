7d3066c44138e6bcff6c668fef923683613da980
ajsen <ajsmartmin@gmail.com>
2023-10-05 03:16:28 +0900

    Test: Minor edits

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,29 +1,28 @@
 import path from 'node:path';
 import fs from 'node:fs';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const outputFormats = ['stylish', 'plain', 'json'];
+const formatNames = ['stylish', 'plain', 'json'];
 const dataFormats = ['json', 'yaml', 'yml'];
 
-describe.each(outputFormats)('Check %s output format', (outputFormat) => {
+describe.each(formatNames)('Check %s output format', (formatName) => {
   test.each(dataFormats)('Check %s data format', (dataFormat) => {
-    const inputPath1 = getFixturePath(`file-1.${dataFormat}`);
-    const inputPath2 = getFixturePath(`file-2.${dataFormat}`);
-    const expectedOutput = readFixture(`${outputFormat}.txt`);
-    const result = genDiff(inputPath1, inputPath2, outputFormat);
-    expect(result).toEqual(expectedOutput);
+    const filePath1 = getFixturePath(`file-1.${dataFormat}`);
+    const filePath2 = getFixturePath(`file-2.${dataFormat}`);
+    const expected = readFixture(`${formatName}.txt`);
+    const result = genDiff(filePath1, filePath2, formatName);
+    expect(result).toEqual(expected);
   });
 });
 
-test('Test unsupported data format', () => {
-  const path1 = getFixturePath('file-1.json');
-  const path2 = getFixturePath('unsupported_format.txt');
-  expect(() => genDiff(path1, path2)).toThrow(new Error('Unsupported data format'));
+test('Check unsupported data format', () => {
+  const filePath = getFixturePath('unsupported_format.txt');
+  const expected = new Error('Unsupported data format');
+  expect(() => genDiff(filePath, filePath)).toThrow(expected);
 });
