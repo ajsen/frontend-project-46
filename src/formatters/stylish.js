f8c78dd4b3d20f096b4aa1e81e5a52e63c2f4e22
ajsen <ajsmartmin@gmail.com>
2023-10-06 00:38:13 +0900

    Refactor: Rename makeIndent

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,7 +4,7 @@ const spaceCount = 4;
 const spaceChar = ' ';
 const separator = '\n';
 
-const makeIndent = (depth, flag) => {
+const setIndentation = (depth, flag) => {
   const offsetCount = (flag) ? 2 : 0;
   return spaceChar.repeat(depth * spaceCount - offsetCount);
 };
@@ -15,10 +15,10 @@ const stringify = (data, depth) => {
   }
   const entries = Object.entries(data);
   const result = entries.map(([key, value]) => ((_.isObject(value))
-    ? `${makeIndent(depth + 1)}${key}: ${stringify(value, depth + 1)}`
-    : `${makeIndent(depth + 1)}${key}: ${value}`));
+    ? `${setIndentation(depth + 1)}${key}: ${stringify(value, depth + 1)}`
+    : `${setIndentation(depth + 1)}${key}: ${value}`));
 
-  return `{\n${result.join(separator)}\n${makeIndent(depth)}}`;
+  return `{\n${result.join(separator)}\n${setIndentation(depth)}}`;
 };
 
 export default (data) => {
@@ -27,24 +27,24 @@ export default (data) => {
       key, value, originalValue, newValue, flag,
     }) => {
       if (flag === 'nested') {
-        return [`${makeIndent(depth + 1)}${key}: ${iter(value, depth + 1)}`];
+        return [`${setIndentation(depth + 1)}${key}: ${iter(value, depth + 1)}`];
       }
       if (flag === 'deleted') {
-        return [`${makeIndent(depth + 1, flag)}- ${key}: ${stringify(value, depth + 1)}`];
+        return [`${setIndentation(depth + 1, flag)}- ${key}: ${stringify(value, depth + 1)}`];
       }
       if (flag === 'added') {
-        return [`${makeIndent(depth + 1, flag)}+ ${key}: ${stringify(value, depth + 1)}`];
+        return [`${setIndentation(depth + 1, flag)}+ ${key}: ${stringify(value, depth + 1)}`];
       }
       if (flag === 'changed') {
         return [
-          `${makeIndent(depth + 1, flag)}- ${key}: ${stringify(originalValue, depth + 1)}`,
-          `${makeIndent(depth + 1, flag)}+ ${key}: ${stringify(newValue, depth + 1)}`,
+          `${setIndentation(depth + 1, flag)}- ${key}: ${stringify(originalValue, depth + 1)}`,
+          `${setIndentation(depth + 1, flag)}+ ${key}: ${stringify(newValue, depth + 1)}`,
         ];
       }
-      return [`${makeIndent(depth + 1)}${key}: ${stringify(value, depth)}`];
+      return [`${setIndentation(depth + 1)}${key}: ${stringify(value, depth)}`];
     });
 
-    return `{\n${result.join(separator)}\n${makeIndent(depth)}}`;
+    return `{\n${result.join(separator)}\n${setIndentation(depth)}}`;
   };
 
   return iter(data, 0);
