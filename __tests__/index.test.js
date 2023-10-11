a1770094efdfc1787f9a98cb12eb1dd4e517c40e
ajsen <ajsmartmin@gmail.com>
2023-10-11 20:02:32 +0900

    Test: Remove the nested loop

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,16 +8,31 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const formatNames = ['stylish', 'plain', 'json'];
 const dataFormats = ['json', 'yaml', 'yml'];
 
-describe.each(formatNames)('Check %s output format', (formatName) => {
-  test.each(dataFormats)('Check %s data format', (dataFormat) => {
-    const filePath1 = getFixturePath(`file-1.${dataFormat}`);
-    const filePath2 = getFixturePath(`file-2.${dataFormat}`);
-    const expected = readFixture(`${formatName}-result.txt`);
-    const result = genDiff(filePath1, filePath2, formatName);
-    expect(result).toEqual(expected);
+const expectedStylish = readFixture('stylish-result.txt');
+const expectedPlain = readFixture('plain-result.txt');
+const expectedJSON = readFixture('json-result.txt');
+
+describe.each(dataFormats)('Check %s format', (dataFormat) => {
+  const filePath1 = getFixturePath(`file-1.${dataFormat}`);
+  const filePath2 = getFixturePath(`file-2.${dataFormat}`);
+
+  test('Should be work', () => {
+    const result = genDiff(filePath1, filePath2);
+    expect(result).toEqual(expectedStylish);
+  });
+  test('Check stylish output', () => {
+    const result = genDiff(filePath1, filePath2, 'stylish');
+    expect(result).toEqual(expectedStylish);
+  });
+  test('Check plain output', () => {
+    const result = genDiff(filePath1, filePath2, 'plain');
+    expect(result).toEqual(expectedPlain);
+  });
+  test('Check plain output', () => {
+    const result = genDiff(filePath1, filePath2, 'json');
+    expect(result).toEqual(expectedJSON);
   });
 });
 
