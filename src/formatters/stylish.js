ff6fa74e9b610fe9652e3c1d48c816d9d37c6fa2
ajsen <ajsmartmin@gmail.com>
2023-10-17 14:54:20 +0900

    Refactor: Change setIndentation

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,13 +1,11 @@
 import _ from 'lodash';
 
 const spaceCount = 4;
+const offsetCount = 2;
 const spaceChar = ' ';
 const separator = '\n';
 
-const setIndentation = (depth, type) => {
-  const offsetCount = (type) ? 2 : 0;
-  return spaceChar.repeat(depth * spaceCount - offsetCount);
-};
+const setIndentation = (depth, space, offset = 0) => spaceChar.repeat(depth * space - offset);
 
 const stringify = (data, depth) => {
   if (!_.isObject(data)) {
@@ -15,35 +13,35 @@ const stringify = (data, depth) => {
   }
   const entries = Object.entries(data);
   const result = entries.map(([key, value]) => ((_.isObject(value))
-    ? `${setIndentation(depth + 1)}${key}: ${stringify(value, depth + 1)}`
-    : `${setIndentation(depth + 1)}${key}: ${value}`));
+    ? `${setIndentation(depth + 1, spaceCount)}${key}: ${stringify(value, depth + 1)}`
+    : `${setIndentation(depth + 1, spaceCount)}${key}: ${value}`));
 
-  return `{\n${result.join(separator)}\n${setIndentation(depth)}}`;
+  return `{\n${result.join(separator)}\n${setIndentation(depth, spaceCount)}}`;
 };
 
 export default (diff) => {
   const iter = (tree, depth) => {
     const result = tree.flatMap((node) => {
       if (node.type === 'deleted') {
-        return [`${setIndentation(depth + 1, node.type)}- ${node.key}: ${stringify(node.value, depth + 1)}`];
+        return [`${setIndentation(depth + 1, spaceCount, offsetCount)}- ${node.key}: ${stringify(node.value, depth + 1)}`];
       }
       if (node.type === 'added') {
-        return [`${setIndentation(depth + 1, node.type)}+ ${node.key}: ${stringify(node.value, depth + 1)}`];
+        return [`${setIndentation(depth + 1, spaceCount, offsetCount)}+ ${node.key}: ${stringify(node.value, depth + 1)}`];
       }
       if (node.type === 'changed') {
         return [
-          `${setIndentation(depth + 1, node.type)}- ${node.key}: ${stringify(node.value1, depth + 1)}`,
-          `${setIndentation(depth + 1, node.type)}+ ${node.key}: ${stringify(node.value2, depth + 1)}`,
+          `${setIndentation(depth + 1, spaceCount, offsetCount)}- ${node.key}: ${stringify(node.value1, depth + 1)}`,
+          `${setIndentation(depth + 1, spaceCount, offsetCount)}+ ${node.key}: ${stringify(node.value2, depth + 1)}`,
         ];
       }
       if (node.type === 'unchanged') {
-        return [`${setIndentation(depth + 1)}${node.key}: ${stringify(node.value, depth)}`];
+        return [`${setIndentation(depth + 1, spaceCount)}${node.key}: ${stringify(node.value, depth)}`];
       }
 
-      return [`${setIndentation(depth + 1)}${node.key}: ${iter(node.children, depth + 1)}`];
+      return [`${setIndentation(depth + 1, spaceCount)}${node.key}: ${iter(node.children, depth + 1)}`];
     });
 
-    return `{\n${result.join(separator)}\n${setIndentation(depth)}}`;
+    return `{\n${result.join(separator)}\n${setIndentation(depth, spaceCount)}}`;
   };
 
   return iter(diff, 0);
