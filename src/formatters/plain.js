203abed41db84d74fb6aff384873a8c75a2f88a6
Ajsen Martynov <ajsmartmin@gmail.com>
2023-10-20 13:23:47 +0900

    Refactor: rename acc to path

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -13,21 +13,21 @@ const stringify = (value) => {
 };
 
 export default (diff) => {
-  const iter = (tree, acc) => {
+  const iter = (tree, path) => {
     const result = tree
       .filter((node) => node.type !== 'unchanged')
       .map((node) => {
         if (node.type === 'deleted') {
-          return `Property '${buildPath(acc, node.key)}' was removed`;
+          return `Property '${buildPath(path, node.key)}' was removed`;
         }
         if (node.type === 'added') {
-          return `Property '${buildPath(acc, node.key)}' was added with value: ${stringify(node.value)}`;
+          return `Property '${buildPath(path, node.key)}' was added with value: ${stringify(node.value)}`;
         }
         if (node.type === 'changed') {
-          return `Property '${buildPath(acc, node.key)}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
+          return `Property '${buildPath(path, node.key)}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
         }
 
-        return iter(node.children, acc.concat(node.key));
+        return iter(node.children, path.concat(node.key));
       });
 
     return result.join(separator);
